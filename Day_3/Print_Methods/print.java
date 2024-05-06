27ee7a564390c1359f1edca50f476a66da9e4550
batahualpa11 <brianatahualpa@gmail.com>
2024-05-06 10:56:01 -0400

    created print.java

--- Day_3/Print_Methods/print.java
+++ Day_3/Print_Methods/print.java
@@ -1,7 +1,7 @@
-public class Double_Quotes {
+public class print {
     public static void main(String[] args) {
         System.out.print("I like to go on walks");
-        System.out.print("I also like to walk my dog")
+        System.out.print(" I also like to walk my dog ");
     }
 }
 
