dffa757b1d915551be7a857486e32ecf5cfce260
batahualpa11 <brianatahualpa@gmail.com>
2024-05-06 10:16:08 -0400

    updated double_quotes.java

--- Day_3/double_quotes.java
+++ Day_3/double_quotes.java
@@ -4,6 +4,7 @@ public class double_quotes {
 
     public static void main(String[] args) {
         System.out.println("This sentance works");
+        System.out.println(This will not give an output); // needs to have double quotes to have an output
     }
 
     
