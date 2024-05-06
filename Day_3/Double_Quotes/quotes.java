37128e1e0339edd6070aef2528c5a5a122780219
batahualpa11 <brianatahualpa@gmail.com>
2024-05-06 10:41:23 -0400

    reorganized

--- Day_3/Double_Quotes/quotes.java
+++ Day_3/Double_Quotes/quotes.java
@@ -1,3 +1,4 @@
+package Day_3.Double_Quotes;
 public class quotes {
     public static void main(String[] args) {
         System.out.println("This is the output with a double quote"); // this will have a seperate execution file
