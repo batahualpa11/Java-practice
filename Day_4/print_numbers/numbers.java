a8ab747f8edbfcdd3f9544bacd2024f10e5f11e0
batahualpa11 <brianatahualpa@gmail.com>
2024-05-07 11:58:29 -0400

    updated numbers.java

--- Day_4/print_numbers/numbers.java
+++ Day_4/print_numbers/numbers.java
@@ -1,7 +1,7 @@
 public class numbers {
     public static void main(String[] args) {
-        System.out.println(256); // will print 
-        System.out.print(256);
+        System.out.println(256); // will print number on one line
+        System.out.print(256); // will print number on seperate line
     }
     
 }
