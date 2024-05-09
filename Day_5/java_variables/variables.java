765d0aa2cbf50039be0c2d90bcad29db779aa8bd
batahualpa11 <brianatahualpa@gmail.com>
2024-05-08 21:31:38 -0400

    update

--- Day_5/java_variables/variables.java
+++ Day_5/java_variables/variables.java
@@ -1,6 +1,6 @@
 public class variables {
     public static void main(String[] args) {
-        String name = "John";
+        String name = "John"; // the variable name claims John and is in form of a string type variable
         System.out.println(name); // this line take the input of the "name" variable and output "john"
     }
 }
