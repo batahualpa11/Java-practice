c005a7b4994c7e7c69d24191afc907b7f68470cc
batahualpa11 <brianatahualpa@gmail.com>
2024-05-08 21:14:37 -0400

    updated

--- Day_5/java_variables/variables.java
+++ Day_5/java_variables/variables.java
@@ -3,6 +3,6 @@ package Day_5.java_variables;
 public class variables {
     public static void main(String[] args) {
         String name = "John"
-        System.out.println(name); // this 
+        System.out.println(name); // this line take the input of the "name" variable and output "john"
     }
 }
