533e5ea9bb221727e103e04fc2a2f7ff986991a5
batahualpa11 <brianatahualpa@gmail.com>
2024-05-14 15:36:23 -0400

    create print_name.java

---
+++ Day_5/print_variables/print_name.java
@@ -0,0 +1,10 @@
+public class print_name {
+    public static void main(String[] args) {
+        String firstName =;
+        String lastName = ;
+        firstName = Brian;
+        lastName = Atahualpa;
+        System.out.println(firstName + lastName);
+    }
+    
+}
