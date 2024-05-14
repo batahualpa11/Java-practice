421c669682efa9c949b806bc6a8f4163f47fb554
batahualpa11 <brianatahualpa@gmail.com>
2024-05-14 15:23:54 -0400

    create print_variables.java

--- Day_5/java_variables/variables.java
+++ Day_5/java_variables/variables.java
@@ -2,5 +2,14 @@ public class variables {
     public static void main(String[] args) {
         String name = "John"; // the variable name claims John and is in form of a string type variable
         System.out.println(name); // this line take the input of the "name" variable and output "john"
+
+        // String = stores text, such as names or text like hello or john 
+
+        // int = stroes whole numbers (integers)
+
+        //
+
+
+
     }
 }
---
+++ Day_5/print_variables/print_var.java
@@ -0,0 +1,7 @@
+public class print_var {
+    public static void main(String[] args) {
+        String name = "Brian";
+        System.out.println("Hello " + name);
+    }
+    
+}
