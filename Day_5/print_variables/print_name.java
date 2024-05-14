700d5922969be2fa56269f94864647d136fabca5
batahualpa11 <brianatahualpa@gmail.com>
2024-05-14 15:59:15 -0400

    update print_name.java

--- Day_5/print_variables/print_name.java
+++ Day_5/print_variables/print_name.java
@@ -1,7 +1,8 @@
 public class print_name {
     public static void main(String[] args) {
-        String firstName = "Brian";
-        String lastName = "Atahualpa";
+       
+        String firstName = "Angelina";
+        String lastName = "Mustafa";
         String fullName = firstName + lastName;
         System.out.println(fullName);
     }
