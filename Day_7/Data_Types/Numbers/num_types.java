09ed469eaffbecb8b47974aabde8bb3299475de5
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 13:33:29 -0400

    create num_types.java

---
+++ Day_7/Data_Types/Numbers/num_types.java
@@ -0,0 +1,20 @@
+public class num_types {
+    public static void main(String[] args) {
+        byte myNum = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+
+        float myFloat = 3.14515f;
+        double myDouble = 9.99d;
+
+        System.out.println(myNum);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+
+    }
+
+}
