ad0125ced7f5c193d56737cede4c33abd60fd97e
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 12:46:19 -0400

    create calc_area.java

---
+++ Day_6/calc_area.java
@@ -0,0 +1,13 @@
+// In this real-life example, we create a program to calculate the area of a rectangle (by multiplying the length and width):
+
+public class calc_area {
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 5;
+        int area;
+
+        area = length * width;
+        System.out.println(area);
+    }
+    
+}
