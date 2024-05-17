07b282de4d610dde36598b484b862d3a934f4d91
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 16:09:16 -0400

    created widen_cast.java

---
+++ Day_7/Data_Types/Casting/widen_cast.java
@@ -0,0 +1,10 @@
+public class widen_cast {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+    
+}
