8b0c3dbe1e669594c5889120ef85f530288a9057
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 11:52:08 -0400

    Update multiple_var.java

--- Day_6/multiple_var.java
+++ Day_6/multiple_var.java
@@ -1,6 +1,6 @@
 public class multiple_var {
     public static void main(String[] args) {
-        int x = 3, y = 4, z = 5;
+        int x = 3, y = 4, z = 5; // allows you to declare multiple variables
         System.out.println(x + y + z);
     }
     
