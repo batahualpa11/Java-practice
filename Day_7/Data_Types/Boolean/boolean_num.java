f6db9fe1b0e58707d41f4c887e9e6d33f7a181f3
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 15:41:24 -0400

    created boolean_num.java

---
+++ Day_7/Data_Types/Boolean/boolean_num.java
@@ -0,0 +1,9 @@
+public class boolean_num {
+    public static void main(String[] args) {
+        boolean areCookiesYummy = true;
+        boolean isOatMealTasty = false;
+
+        System.out.println(areCookiesYummy);
+        System.out.println(isOatMealTasty);
+    }
+}
