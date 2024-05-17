157c21fcb0dd6ee9f69803b82a05b9d3c68a1ef2
batahualpa11 <brianatahualpa@gmail.com>
2024-05-17 16:20:10 -0400

    created narrow_cast.java

---
+++ Day_7/Data_Types/Casting/narrow_cast.java
@@ -0,0 +1,8 @@
+public class narrow_cast {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // manual casting: double to int
+        System.out.println(myInt);
+    }
+    
+}
--- Day_7/Data_Types/Casting/widen_cast.java
+++ Day_7/Data_Types/Casting/widen_cast.java
@@ -1,7 +1,7 @@
 public class widen_cast {
     public static void main(String[] args) {
         int myInt = 9;
-        double myDouble = myInt;
+        double myDouble = myInt; // automatic casting: changing int to double
 
         System.out.println(myInt);
         System.out.println(myDouble);
