41939b6a069ba2a47cf78bb2762803152673337f
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:38:39 -0400

    update sqrt.java

--- Day_8/Math/sqrt.java
+++ Day_8/Math/sqrt.java
@@ -1,7 +1,7 @@
 public class sqrt {
     public static void main(String[] args) {
         Math.sqrt(36); // this uses the math class and sqrt method to take the input of 36 to sqrt 
-        System.out.println(Math.sqrt(36)); // this line then prints out the value of the squareroot of 36 whic is 6 
+        System.out.println(Math.sqrt(36)); // this line then prints out the value of the squareroot of 36 which is 6 
     }
     
 }
