9ac7262787e3643b2cb0030446b171f33d7a1d0f
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:44:52 -0400

    updated absolute.java

--- Day_8/Math/absolute.java
+++ Day_8/Math/absolute.java
@@ -1,7 +1,7 @@
 public class absolute {
     public static void main(String[] args) {
-        Math.abs(-3.67);
-        System.out.println(Math.abs(-3.67));
+        Math.abs(-3.67); // this will always print out the positive number
+        System.out.println(Math.abs(-3.67)); // outputs the absolute value
     }
     
 }
