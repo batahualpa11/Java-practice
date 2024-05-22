9498adbe462849e03f29c8125f1356a4708cb355
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:25:52 -0400

    created min.java

---
+++ Day_8/Math/min.java
@@ -0,0 +1,6 @@
+public class min {
+    public static void main(String[] args) {
+        Math.min(20, 80); // this uses the class Math and is using the method min to find the minimum value for x or y 
+        System.out.println(Math.min(20, 80)); // this should then print out the least number which is 20 for x 
+    }
+}
