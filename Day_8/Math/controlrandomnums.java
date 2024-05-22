07dd1d9ff10e5cf732a504d79514d6ad3c3125ae
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 15:16:24 -0400

    updated controlrandomnums.java

---
+++ Day_8/Math/controlrandomnums.java
@@ -0,0 +1,7 @@
+public class controlrandomnums {
+    public static void main(String[] args) {
+        int randomNum = (int)(Math.random() * 101); // this makes sure that the random number is an integer 
+        System.out.println(randomNum); // the randomNum is an integer 
+    }
+    
+}
