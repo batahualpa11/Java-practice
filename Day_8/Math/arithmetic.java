4426a434ec0873c6b57823086b838068cab41a73
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:11:44 -0400

    created arithmetic.java

---
+++ Day_8/Math/arithmetic.java
@@ -0,0 +1,7 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        Math.max(5,10); // this prints out the highest value of x and y. An example would be Math.max(x,y);
+        System.out.println(Math.max(5,10)); // this should then print 10 since its the highest number
+    }
+    
+}
