982f15a1dc539fbf4d99b5268aae947dffffcd75
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 19:27:08 -0400

    create votingprog.java

---
+++ Day_8/Boolean_Expression/votingprog.java
@@ -0,0 +1,13 @@
+public class votingprog {
+    public static void main(String[] args) {
+        int myAge = 16;
+        int ageLimit = 18;
+
+        if (myAge >= ageLimit) {
+            System.out.println("You are able to vote!");
+        }
+        else {
+            System.out.println("you CANNOT vote!!");
+        }
+    }
+}
