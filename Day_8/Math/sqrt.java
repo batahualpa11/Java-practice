891eac1b7bfdd1324b00ad9682c84cdc9e7784df
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:37:10 -0400

    update sqrt.java

--- Day_8/Math/sqrt.java
+++ Day_8/Math/sqrt.java
@@ -1,7 +1,7 @@
 public class sqrt {
     public static void main(String[] args) {
-        Math.sqrt(10);
-        System.out.println(Math.sqrt(10));
+        Math.sqrt(10); // this uses the math class and sqrt method to take the input of 10 to sqrt 
+        System.out.println(Math.sqrt(10)); // this line then prints out the value of the squareroot of 10 
     }
     
 }
