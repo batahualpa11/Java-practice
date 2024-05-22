eaf96955b776d35e29832a608c823e0a82b88076
batahualpa11 <brianatahualpa@gmail.com>
2024-05-22 14:54:53 -0400

    created randomnums.java

---
+++ Day_8/Math/randomnums.java
@@ -0,0 +1,7 @@
+public class randomnums {
+    public static void main(String[] args) {
+        Math.random();
+        System.out.println(Math.random()); // prints random numbers between 0.0 (inclusive) and 1.0 (exclusive)
+    }
+    
+}
