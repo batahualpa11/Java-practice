707639a843f49551230074b30585b810f64ec303
batahualpa11 <brianatahualpa@gmail.com>
2024-05-24 17:45:25 -0400

    create ternary_operator.java

---
+++ Day_9/if_else statements/ternary_operator.java
@@ -0,0 +1,8 @@
+public class ternary_operator {
+    public static void main(String[] args) {
+        int time = 30;
+        if (time < 40) {
+            System.out.println("Hello World ");
+        }
+    }
+}
