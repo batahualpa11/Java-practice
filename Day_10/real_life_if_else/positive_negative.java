572bb267d28c701b7f0ded6517d1edc9eeeb0402
batahualpa11 <brianatahualpa@gmail.com>
2024-05-26 12:50:05 -0400

    created positive or negative guessing java program

---
+++ Day_10/real_life_if_else/positive_negative.java
@@ -0,0 +1,16 @@
+// This example shows how you can use if..else to find out if a number is positive or negative:
+
+public class positive_negative {
+    public static void main(String[] args) {
+        int myNum = 4;
+
+        if (myNum < 0) {
+            System.out.println("Your number is a negative!");
+        } else if (myNum > 0) {
+            System.out.println("your number is a positive!!");
+        } else {
+            System.out.println("your number is the same as 0");
+        }
+    }
+    
+}
