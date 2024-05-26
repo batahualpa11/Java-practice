929ed08fe6676255d25eef858040234f49d510f7
batahualpa11 <brianatahualpa@gmail.com>
2024-05-26 12:51:27 -0400

    updated positive_negative.java

--- Day_10/real_life_if_else/positive_negative.java
+++ Day_10/real_life_if_else/positive_negative.java
@@ -2,7 +2,7 @@
 
 public class positive_negative {
     public static void main(String[] args) {
-        int myNum = 2;
+        int myNum = -8;
 
         if (myNum < 0) {
             System.out.println("Your number is a negative!");
