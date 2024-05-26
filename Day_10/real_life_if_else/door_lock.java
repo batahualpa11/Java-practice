481d4cd5ab1df38d91ffe838882b423044ab8305
batahualpa11 <brianatahualpa@gmail.com>
2024-05-26 12:40:54 -0400

    created real life scenerio for if_else statements

---
+++ Day_10/real_life_if_else/door_lock.java
@@ -0,0 +1,11 @@
+public class door_lock {
+    public static void main(String[] args) {
+        int doorCode = 1002;
+
+        if (doorCode == 1002) {
+            System.out.println("Correct the door will now open!!");
+        } else {
+            System.out.println("Wrong!! The door will not open!");
+        }
+    }
+}
