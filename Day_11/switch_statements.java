76c79e3f1ecc9347a84a278d15c2173d9f9d44eb
batahualpa11 <brianatahualpa@gmail.com>
2024-05-27 13:16:28 -0400

    created switch_statements.java

---
+++ Day_11/switch_statements.java
@@ -0,0 +1,35 @@
+// The example below uses the weekday number to calculate the weekday name:
+
+public class switch_statements {
+    public static void main(String[] args) {
+        int day = 4;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                break;
+        }
+        
+    }
+    
+}
