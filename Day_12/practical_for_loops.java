4de680346da203dd1b3d1558fd62cf48e62f66f6
batahualpa11 <brianatahualpa@gmail.com>
2024-05-28 15:10:01 -0400

    created practical_for_loops.java

---
+++ Day_12/practical_for_loops.java
@@ -0,0 +1,9 @@
+// To demonstrate a practical example of the for loop, let's create a program that counts to 100 by tens:
+
+public class practical_for_loops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
