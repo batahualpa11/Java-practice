19867f59c567cb3476e62f44e74a6ba98687d1bc
batahualpa11 <brianatahualpa@gmail.com>
2024-05-28 14:29:58 -0400

    created for loop irl.java

---
+++ Day_12/for_loop_irl.java
@@ -0,0 +1,10 @@
+// This example will only print even values between 0 and 10:
+
+public class for_loop_irl {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+    
+}
