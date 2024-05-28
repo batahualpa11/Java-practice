85d300902719228fe922ebb516ab4a653d8a4a8a
batahualpa11 <brianatahualpa@gmail.com>
2024-05-28 13:01:27 -0400

    created while_loops.java

---
+++ Day_12/while_loops.java
@@ -0,0 +1,11 @@
+public class while_loops {
+    public static void main(String[] args) {
+        int myDay = 0;
+
+        while (myDay < 10) {
+            System.out.println(myDay);
+            myDay++;
+        }
+    }
+    
+}
