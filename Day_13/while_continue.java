7c7f6893fbc2e8bdf488381c4aa3a57760b93196
batahualpa11 <brianatahualpa@gmail.com>
2024-05-29 17:24:52 -0400

    created while_continue.java

---
+++ Day_13/while_continue.java
@@ -0,0 +1,14 @@
+public class while_continue {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 10) {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
