7037037b4e3fbf14495d61b412f622cabd89244c
batahualpa11 <brianatahualpa@gmail.com>
2024-05-29 17:39:02 -0400

    updates

--- Day_13/while_break.java
+++ Day_13/while_break.java
@@ -1,13 +1,9 @@
 public class while_break {
     public static void main(String[] args) {
-        
         int i = 0;
-
         while (i < 10) {
-
             System.out.println(i);
             i++;
-
             if (i == 8) {
                 break;
             }
--- Day_13/while_continue.java
+++ Day_13/while_continue.java
@@ -1,7 +1,6 @@
 public class while_continue {
     public static void main(String[] args) {
         int i = 0;
-
         while (i < 10) {
             if (i == 3) {
                 i++;
