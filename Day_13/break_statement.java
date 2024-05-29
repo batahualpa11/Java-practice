59eb5f23cd0df60c8a947ec0816fd12c617bd372
batahualpa11 <brianatahualpa@gmail.com>
2024-05-29 16:52:10 -0400

    updated break_statement.java

--- Day_13/break_statement.java
+++ Day_13/break_statement.java
@@ -2,7 +2,7 @@ public class break_statement {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             if (i == 4) {
-                continue;
+                break;
             }
             System.out.println(i);
         }
